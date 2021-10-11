15b308a8e83a516d886c008207d5817782c40165
Owen <User@DESKTOP-DU5OO0U>
2021-10-11 17:33:41 +0200

    add

---
+++ Owen-Template/Owen-Template/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Owen_Template.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
